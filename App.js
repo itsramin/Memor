d1f26a9ca28650f24c0447877c5aaa02cc65b9a3
Ramin <Raminshakooei@gmail.com>
2022-10-31 13:29:31 +0330

    init

--- App.js
+++ App.js
@@ -1,71 +1,11 @@
-import { useState } from "react";
-import { Button, FlatList, StatusBar, StyleSheet, View } from "react-native";
-import GoalInput from "./Components/GoalInput";
-import GoalItem from "./Components/GoalItem";
+import { StatusBar, StyleSheet } from "react-native";
 
 export default function App() {
-  const [goalList, setGoalList] = useState([]);
-  const [modalIsVisible, setModalIsVisibel] = useState(false);
-
-  const showModal = () => {
-    setModalIsVisibel(true);
-  };
-  const hideModal = () => {
-    setModalIsVisibel(false);
-  };
-
-  const addGoalHandler = (enteredText) => {
-    setGoalList((prev) => [
-      ...prev,
-      { text: enteredText, id: Math.random().toString() },
-    ]);
-    setModalIsVisibel(false);
-  };
-
-  const deleteGoalHandler = (id) => {
-    setGoalList((prev) => {
-      return prev.filter((item) => item.id !== id);
-    });
-  };
-
   return (
     <>
       <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button title="Add New Goal" color="#a065ec" onPress={showModal} />
-        <GoalInput
-          onAddGoal={addGoalHandler}
-          visible={modalIsVisible}
-          onHide={hideModal}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={goalList}
-            keyExtractor={(item) => item.id}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  id={itemData.item.id}
-                  text={itemData.item.text}
-                  deleteGoal={deleteGoalHandler}
-                />
-              );
-            }}
-          />
-        </View>
-      </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-
-  goalsContainer: {
-    flex: 5,
-  },
-});
+const styles = StyleSheet.create({});
